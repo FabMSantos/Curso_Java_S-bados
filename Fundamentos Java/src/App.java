1341a7720de8b36b747b1d1c0af536aef5d894a0
CaioMTRodrigues <caiom.rodrigues@hotmail.com>
2024-09-21 17:01:11 -0300

    Exec1Java

---
+++ Fundamentos Java/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Olá,Turma!");
+        int a, b, c;
+        a=10;
+        b=20;
+        c = a + b;
+        
+        System.out.println("Valor de C= " + c);
+    }
+}
---
+++ Fundamentos Java/src/Exercicio_escrever_comentar.java
@@ -0,0 +1,26 @@
+public class Exercicio_escrever_comentar {
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo, Eu sou Fabiano");
+
+ 
+        // Exemplo de comentário de uma linha
+
+
+        /*
+        Exemplo de 
+        comentário de
+            múltiplas linhas
+                */
+
+    }
+}
+
+
+
+
+        
+
+
+
+
